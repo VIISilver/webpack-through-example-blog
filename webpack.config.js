d0b0fce0ead37dbd4df0e9148a03dac3c895924f
Kalalau <cantrellkalalau@gmail.com>
2018-01-22 09:40:37 -0500

    simplified webpack config and cleaned up overall formatting

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,6 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        include: path.join(__dirname, 'src/css'),
         use: [
           {
             loader: 'file-loader',
@@ -48,19 +47,23 @@ module.exports = {
             }
           },
           'extract-loader',
-          'css-loader'
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          }
         ]
       },
       {
         test: /\.(png|jpg|gif)$/,
-        include: path.join(__dirname, 'src/imgs'),
         use: {
           loader: 'responsive-loader',
           options: {
             sizes: [300],
             placeholder: true,
             placeholderSize: 50,
-            name: 'imgs/[name]-[hash].[ext]'
+            name: 'imgs/[name]-[width].[ext]'
           }
         }
       }
